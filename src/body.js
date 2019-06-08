1d4efc0f5df6a22353eca719d6ed83ea357fdbad
Jose Marcelo Santuci <jsantuci@MACCIT013994.local>
2019-06-08 13:53:23 -0300

    Unit test are ok!

---
+++ src/body.js
@@ -0,0 +1,12 @@
+
+class Body {
+    constructor(address, message, star) {
+        this.address = address;
+        this.message = message;
+        // this.signature = ;
+        this.star = star;
+    }
+}
+
+
+module.exports.Body = Body;                    // Exposing the Body class as a module
